49ee3216a4e77e629e273f08889a0a26ac45b663
jESsEY7 <ngenojesse@gmail.com>
2026-01-20 01:07:59 +0300

    Configure wagtail-bakery for static site generation on Netlify

--- myproject/settings/base.py
+++ myproject/settings/base.py
@@ -46,6 +46,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "bakery",
+    "wagtailbakery",
 ]
 
 MIDDLEWARE = [
@@ -179,3 +181,9 @@ WAGTAILADMIN_BASE_URL = "http://example.com"
 # if untrusted users are allowed to upload files -
 # see https://docs.wagtail.org/en/stable/advanced_topics/deploying.html#user-uploaded-files
 WAGTAILDOCS_EXTENSIONS = ['csv', 'docx', 'key', 'odt', 'pdf', 'pptx', 'rtf', 'txt', 'xlsx', 'zip']
+
+# Wagtail Bakery (Static Site Generation)
+BUILD_DIR = BASE_DIR / "build"
+BAKERY_VIEWS = (
+    "wagtailbakery.views.AllPublishedPagesView",
+)
